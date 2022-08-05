763bab160290cc95ca04c5fe27aa575b7ebc83fb
UmarEngineer <ohojimirzayev@gmail.com>
2022-08-05 17:56:54 +0500

    Responsive navbar section, add script codes

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+const searchInput = document.querySelector('.searchInput');
+const langChange = document.querySelector('.lang-change');
+const sign = document.querySelector('.sign');
+const navItems = document.querySelector('.nav-items');
+const navTop = document.querySelector('.nav-top');
+const navBottom = document.querySelector('.nav-bottom');
+const navTopRight = document.querySelector('.nav-top__right');
+const navTopLeft = document.querySelector('.nav-top__left');
+const hamburgerIcon = document.querySelector('.hamburgerIcon');
+
+searchInput.addEventListener('focus', () => {
+    searchInput.style.width = '235px'
+    langChange.style.display = 'none'
+    sign.style.display = 'none'
+})
+
+searchInput.addEventListener('blur', () => {
+    searchInput.style.width = '50px'
+    langChange.style.display = 'flex'
+    sign.style.display = 'flex'
+})
+
+function changeIcon(x) {
+    x.classList.toggle("change");
+    navItems.classList.toggle('active')
+}
+
+window.addEventListener('resize', () => {
+    if(window.screen.availWidth < 590){
+        navBottom.appendChild(navTopRight)
+        navTopRight.style.justifyContent = 'center'
+        navTopRight.style.paddingTop = '20px'
+        
+    }
+    else if (window.screen.availWidth > 590){
+        navTop.appendChild(navTopRight)
+        navTop.appendChild(hamburgerIcon)
+    }
+
+})
+
+
