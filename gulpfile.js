e360a96b385b1a21860df422411e96711b621161
UmarEngineer <ohojimirzayev@gmail.com>
2022-08-06 20:09:48 +0500

    Fix some size property

--- gulpfile.js
+++ gulpfile.js
@@ -27,13 +27,13 @@ gulp.task('watch', async function () {
     gulp.watch('source/styles/**/*.css', gulp.series('styles'))
 })
 
-const netlify = require('gulp-netlify')
-gulp.task('deploy', async function () {
-  gulp.src('./dist/**/*')
-    .pipe(netlify({
-      site_id: '2d6359c1-3735-4664-a6ae-3f3a4406a44b',
-      access_token: 'iwiquubxCf5evH58Avca4Hijxe9lk6y7lnhH1I9lvwY'
-    }))
-})
+// const netlify = require('gulp-netlify')
+// gulp.task('deploy', async function () {
+//   gulp.src('./dist/**/*')
+//     .pipe(netlify({
+//       site_id: '2d6359c1-3735-4664-a6ae-3f3a4406a44b',
+//       access_token: 'iwiquubxCf5evH58Avca4Hijxe9lk6y7lnhH1I9lvwY'
+//     }))
+// })
 
-gulp.task('default', gulp.series('styles', 'deploy', 'scripts', 'watch'))
+gulp.task('default', gulp.series('styles', 'scripts', 'watch'))
